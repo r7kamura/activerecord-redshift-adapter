63d2858f54564508afbe75a4561fd8c0440bfd18
r7kamura <r7kamura@gmail.com>
2025-10-15 16:15:13 +0900

    Support beta and rc versions

--- lib/active_record/connection_adapters/redshift_adapter.rb
+++ lib/active_record/connection_adapters/redshift_adapter.rb
@@ -4,15 +4,18 @@ require 'pg'
 
 require_relative "../tasks/redshift_tasks"
 
-if ActiveRecord.version >= Gem::Version.new('8.1.0')
+error_message = 'no compatible version of ActiveRecord detected'
+
+case [ActiveRecord::VERSION::MAJOR, ActiveRecord::VERSION::MINOR]
+when [8, 1]
   require_relative 'redshift_8_1_adapter'
-elsif ActiveRecord.version >= Gem::Version.new('8.0.0')
+when [8, 0]
   require_relative 'redshift_8_0_adapter'
-elsif ActiveRecord.version >= Gem::Version.new('7.2.0')
+when [7, 2]
   require_relative 'redshift_7_2_adapter'
-elsif ActiveRecord.version >= Gem::Version.new('7.1.0')
+when [7, 1]
   require_relative 'redshift_7_1_adapter'
-elsif ActiveRecord.version >= Gem::Version.new('7.0.0')
+when [7, 0]
   require_relative 'redshift_7_0_adapter'
 else
   raise 'no compatible version of ActiveRecord detected'
